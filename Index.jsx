71dc6e970a849bd9c3061bfc0fa0fc85d6fb69d1
JeanCarlos Jimenez <jeancarlosjimenezc25@gmail.com>
2023-12-09 09:16:34 -0500

    Changes Initial

---
+++ Index.jsx
@@ -0,0 +1,13 @@
+//* Index - Jsx
+
+function Index (){
+    return(
+        <>
+        <div className="app">
+            <h1>Index</h1>
+        </div>
+        </>
+    )
+}
+
+export default Index
